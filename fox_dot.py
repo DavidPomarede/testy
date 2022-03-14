ad7a77cd55d4bbccf42a31e2865aa24bae21eb57
Dave <42557494+DavidPomarede@users.noreply.github.com>
2022-03-14 15:58:44 -0400

    Create fox_dot.py

--- fox_dot.py
+++ fox_dot.py
@@ -10,7 +10,7 @@ def my_buffer_free(buffer):
     print(f"If I needed to, I could release some resources used by {buffer}!")
 
 //Samples.buffer_read = my_buffer_read
-//Samples.buffer_free = my_buffer_free
+Samples.buffer_free = my_buffer_free
 
 kick_drum = Samples.getBufferFromSymbol('x')
 # kick_drum.data contains the sample data as bytes.
