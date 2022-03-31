e4f8e934954c810877b0c657221983e7c9f105a2
Dave <42557494+DavidPomarede@users.noreply.github.com>
2022-03-31 18:25:23 -0400

    Update fox_dot.py

--- fox_dot.py
+++ fox_dot.py
@@ -11,7 +11,7 @@ def my_buffer_free(buffer):
 
  p1 >> pads([0,1,2,3], dur=[1/4,3/4], sus=1, vib=4, scale=Scale.minor)   
  p2 >> pads([0,6,2,3], dur=[1/4,3/4], sus=1, vib=4, scale=Scale.minor)   
-
+ b1 >> drums([0,6,2,3], dur=[1/4,3/4], sus=1, vib=4, scale=Scale.minor)  
 
 Samples.buffer_read = my_buffer_read
 Samples.buffer_free = my_buffer_free
