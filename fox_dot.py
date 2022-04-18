6f6e1df15fc2b8e1bdfe1f2f6008509e0b338574
Dave <42557494+DavidPomarede@users.noreply.github.com>
2022-04-18 13:03:48 -0400

    Update fox_dot.py

--- fox_dot.py
+++ fox_dot.py
@@ -14,7 +14,7 @@ def my_buffer_free(buffer):
  b1 >> drums([0,6,2,3], dur=[1/4,3/4], sus=1, vib=4, scale=Scale.minor)  
 
 Samples.buffer_read = my_buffer_read
-//Samples.buffer_free = my_buffer_free
+Samples.buffer_free = my_buffer_free
 
 kick_drum = Samples.getBufferFromSymbol('x')
 # kick_drum.data contains the sample data as bytes.
